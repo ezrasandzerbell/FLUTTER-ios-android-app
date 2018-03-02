0c88d4f95653752667cbedf895f03eb67f562a7e
Ezra <ezra@Ezra-Mac-Mini.local>
2018-03-02 11:52:08 -0800

    refactor logo with its own widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,55 +8,56 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
-    Widget titleSection = new Container(
-      padding: const EdgeInsets.all(32.0),
-      child: new Row(
-        children: [
-          new Expanded(
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                new Container(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: new Text(
-                    'Oeschinen Lake Campground',
-                    style: new TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                new Text(
-                  'Kandersteg, Switzerland',
-                  style: new TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          new Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          new Text('41'),
-        ],
+
+    Widget style360Logo = new Container(
+      padding: const EdgeInsets.only(top: 100.0),
+      child: new Image.asset(
+        'images/style.png',
+        width: 180.0,
+        height: 180.0,
       ),
     );
+
+    Widget titleTextSection = new Container(
+      padding: const EdgeInsets.only(left: 80.0, right: 80.0, top: 30.0, bottom: 20.0),
+        child: new Text(
+        ''' Welcome to Style360''',
+            textAlign: TextAlign.center,
+        style: new TextStyle(
+          fontSize: 35.0,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+
+    Widget subtitleTextSection = new Container(
+      padding: const EdgeInsets.only(left: 40.0, right: 40.0, bottom: 40.0),
+      child: new Text(
+        '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet Donec imperdiet.''',
+        textAlign: TextAlign.center,
+        style: new TextStyle(
+            fontSize: 15.0,
+            fontWeight: FontWeight.w400
+        ),
+      ),
+    );
+
+
+//    footer
     Column signInLink(String label) {
 
       return new Column(
-        mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           new Container(
-            margin: const EdgeInsets.only(top: 8.0),
             child: new Text(
               label,
               style: new TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.w400,
-                color: const Color(0xFFf735e9)
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w400,
+                  color: const Color(0xFFf735e9)
               ),
             ),
           ),
@@ -68,15 +69,13 @@ class MyApp extends StatelessWidget {
     Column existingAccountLink(String label) {
 
       return new Column(
-        mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           new Container(
-            margin: const EdgeInsets.only(top: 8.0),
             child: new Text(
               label,
               style: new TextStyle(
-                  fontSize: 12.0,
+                  fontSize: 16.0,
                   fontWeight: FontWeight.w400,
                   color: const Color(0xFFeceff1)
               ),
@@ -86,7 +85,7 @@ class MyApp extends StatelessWidget {
       );
     }
 
-    Widget bottomNav = new Container(
+    Widget footer = new Container(
       height: 50.0,
       decoration: new BoxDecoration(color: Colors.black),
       child: new Row(
@@ -98,47 +97,15 @@ class MyApp extends StatelessWidget {
       ),
     );
 
-
-    Widget titleTextSection = new Container(
-      padding: const EdgeInsets.only(left: 102.0, right: 102.0, top: 30.0, bottom: 30.0),
-        child: new Text(
-        ''' Welcome to Style360''',
-        softWrap: true,
-            textAlign: TextAlign.center,
-        style: new TextStyle(
-          fontSize: 25.0,
-          fontWeight: FontWeight.w400,
-        ),
-      ),
-    );
-
-    Widget subtitleTextSection = new Container(
-      padding: const EdgeInsets.only(left: 40.0, right: 40.0, bottom: 40.0),
-      child: new Text(
-        '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet Donec imperdiet.''',
-        softWrap: true,
-        textAlign: TextAlign.center,
-        style: new TextStyle(
-            fontSize: 15.0,
-            fontWeight: FontWeight.w400
-        ),
-      ),
-    );
-
     return new MaterialApp(
       title: 'Flutter Demo',
       home: new Scaffold(
         body: new ListView(
-          padding: const EdgeInsets.only(top: 100.0),
           children: [
-            new Image.asset(
-              'images/style.png',
-              width: 150.0,
-              height: 150.0,
-            ),
+            style360Logo,
             titleTextSection,
             subtitleTextSection,
-            bottomNav,
+            footer,
           ],
         ),
       ),
